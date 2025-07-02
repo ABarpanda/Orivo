a830309cc3b336728d7f67fd44cfba1f920f91a6
Amritanshu Barpanda <145291762+ABarpanda@users.noreply.github.com>
2025-07-02 12:17:03 +0530

    folder refactor

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import joblib
 from typing import Final
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-from responses import get_response
-from keep_alive import keep_alive
+# from functions.responses import get_response
+from functions.keep_alive import keep_alive
 from better_profanity import profanity
 keep_alive()
 
@@ -16,23 +16,23 @@ intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
 
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('(Message was empty because intents were not enabled probably)')
-        return
+# async def send_message(message: Message, user_message: str) -> None:
+#     if not user_message:
+#         print('(Message was empty because intents were not enabled probably)')
+#         return
 
-    if user_message[0] == '?':
-        is_private = True
-    else: is_private = False
+#     if user_message[0] == '?':
+#         is_private = True
+#     else: is_private = False
 
-    if is_private:
-        user_message = user_message[1:]
+#     if is_private:
+#         user_message = user_message[1:]
 
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+#     try:
+#         response: str = get_response(user_message)
+#         await message.author.send(response) if is_private else await message.channel.send(response)
+#     except Exception as e:
+#         print(e)
 
 clf = joblib.load('spam_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
