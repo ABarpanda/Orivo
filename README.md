2f89de5531d4795e6279b7a6717d68a89a605ba5
Amritanshu Barpanda <145291762+ABarpanda@users.noreply.github.com>
2025-08-22 07:22:41 +0530

    Revert "Added gunicorn for render"
    This reverts commit f9f8110ab005dba8f2d56296997aabe1cc03e682.

--- README.md
+++
@@ -1,4 +0,0 @@
-## main.py
-
-keep_alive() is used to keep the script running continuously. 
-load_dotenv() is used to load the secret keys from .env file 
--- functions/keep_alive.py
+++ functions/keep_alive.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from threading import Thread
 
@@ -9,8 +8,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
